de8d40483a07890655cb5a5df83a98d019bf2ddb
Arda Karşı <ardakarsi03@gmail.com>
2026-04-05 21:38:04 +0200

    physmemmanager.cc and .h implemented. opened up .h to add a hole-list data structure and .cc to initialize a big free hole and implement allocate and release Pages

--- os/physmemmanager.cc
+++ os/physmemmanager.cc
@@ -130,6 +130,7 @@ PhysMemManagerHole::PhysMemManagerHole(const uint64_t pageSize,
                                        const uint64_t memorySize)
   : PhysMemManager(pageSize, memorySize)
 {
+  holes.push_back({0, nPages});
 }
 
 /* Allocate @count pages, returning the starting address in @addr.
@@ -140,9 +141,26 @@ PhysMemManagerHole::allocatePages(size_t count, uintptr_t &addr)
 {
   /* Check upfront if pages are available at all. */
   if (nAllocatedPages + count > nPages)
-      return false;
+    return false;
 
-  /* TODO: Implement. */
+  for(auto it = holes.begin(); it != holes.end(); ++it){
+    if(it->len < count)
+      continue;
+    uint64_t spoint = it->spoint;
+    addr = (uintptr_t)baseAddress + spoint * pageSize;
+
+    it->spoint += count;
+    it->len -= count;
+
+    if(it->len == 0)
+      holes.erase(it);
+
+    nAllocatedPages += count;
+    maxAllocatedPages = std::max(maxAllocatedPages, nAllocatedPages);
+
+    return true;
+  }
+
   return false;
 }
 
@@ -152,4 +170,29 @@ void
 PhysMemManagerHole::releasePages(uintptr_t addr, size_t count)
 {
   /* TODO: Implement. */
+  uint64_t spoint = (addr - (uintptr_t)baseAddress) / pageSize;
+  Hole released{spoint, count};
+
+  auto it = holes.begin();
+  while(it != holes.end() && it->spoint < spoint){
+    ++it;
+  }
+
+  auto inp = holes.insert(it, released);
+  if(inp != holes.begin()){
+    auto prev = std::prev(inp);
+
+    if(prev->spoint + prev->len == inp->spoint){
+      prev->len += inp->len;
+      inp = holes.erase(inp);
+      inserted = prev;
+    }
+  }
+
+  auto next = std::next(inserted);
+  if(next != holes.end() && inserted->spoint + inserted->len == next->spoint){
+    inserted->len += next->len;
+    holes.erase(next);
+  }
+  nAllocatedPages -= count;
 }
